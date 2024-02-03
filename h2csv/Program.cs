49ab5e6db2a27190cc66786d876673c99ea78cf2
codewitch-honey-crisis <gaztoast@gmail.com>
2024-02-03 10:38:05 -0800

    Add project files.

---
+++ h2csv/Program.cs
@@ -0,0 +1,218 @@
+﻿using VisualFA;
+
+internal partial class Program
+{
+    [CmdArg(Ordinal = 0)]
+    static TextReader Input { get; set; } = Console.In;
+    [CmdArg]
+    static TextWriter Output { get; set; } = Console.Out;
+    struct _Field
+    {
+        public string RawType;
+        public string Name;
+        public int RawLength;
+    }
+    static void _Expecting(FAMatch match, params int[] ids)
+    {
+        int i;
+        for(i  = 0; i < ids.Length;++i)
+        {
+            if (match.SymbolId == ids[i])
+                break;
+        }
+        if(i==ids.Length)
+        {
+            throw new Exception(string.Format("Unexpected symbol in input. at line {0}, column {1}",match.Line, match.Column));
+        }
+    }
+    static bool _SkipToId(IEnumerator<FAMatch> e, string id)
+    {
+        while(_SkipTo(e,identifier) && e.Current.Value!=id)
+        {
+            if(!e.MoveNext())
+            {
+                return false;
+            }
+        }
+        return e.Current.SymbolId==identifier && e.Current.Value==id ;
+    }
+    static bool _SkipTo(IEnumerator<FAMatch> e, int id)
+    {
+        var result = true;
+        while (e.Current.SymbolId != id && (result = e.MoveNext())) ;
+        return result;
+    }
+    static void Run()
+    {
+        
+        var structs = new Dictionary<string,List<_Field>>();
+        using (var e = Lex(Input).GetEnumerator())
+        {
+            if(!e.MoveNext())
+            {
+                Console.Error.WriteLine("Header was empty of significant content.");
+                return;
+            }
+            while(_SkipToId(e,"struct"))
+            {
+                if(!e.MoveNext())
+                {
+                    Console.Error.WriteLine("Unterminated struct");
+                    return;
+                }
+                _Expecting(e.Current, identifier);
+                string name = e.Current.Value;
+                _Field f = default;
+                if (!_SkipTo(e,lbrace))
+                {
+                    Console.Error.WriteLine("Expected {");
+                    return;
+                }
+                if(!_SkipTo(e,identifier))
+                {
+                    Console.Error.WriteLine("Unterminated or empty struct");
+                    return;
+                }
+                var fields = new List<_Field>();
+                while (_SkipTo(e,identifier))
+                {
+                    if (e.Current.Value == "constexpr")
+                    {
+                        if (!_SkipTo(e, semi))
+                        {
+                            Console.Error.WriteLine("Unterminated field");
+                            return;
+                            
+                        }
+                        continue;
+                    }
+                    f.RawType = e.Current.Value;
+                    if(!e.MoveNext())
+                    {
+                        Console.Error.WriteLine("Unterminated field");
+                        return;
+                    }
+                    if(!_SkipTo(e,identifier))
+                    {
+                        _Expecting(e.Current, identifier);
+                    }
+                    f.Name = e.Current.Value;
+                    if(!e.MoveNext())
+                    {
+                        Console.Error.WriteLine("Unterminated field");
+                        return;
+                    }
+                    if(e.Current.SymbolId==lbracket)
+                    {
+                        if(!e.MoveNext())
+                        {
+                            Console.Error.WriteLine("Unterminated field");
+                            return;
+                        }
+                        _Expecting(e.Current, integer);
+                        f.RawLength = int.Parse(e.Current.Value);
+                        if (!e.MoveNext())
+                        {
+                            Console.Error.WriteLine("Unterminated field");
+                            return;
+                        }
+                        _Expecting(e.Current, rbracket);
+                        if (!e.MoveNext())
+                        {
+                            Console.Error.WriteLine("Unterminated field");
+                            return;
+                        }
+                        
+                    } else if(e.Current.SymbolId==equals)
+                    {
+                        if(!_SkipTo(e,rbracket))
+                        {
+                            Console.Error.WriteLine("Unterminated struct");
+                            return;
+                        }
+                        name = null;
+                        break;
+                    }
+                    _Expecting(e.Current, semi);
+                    if(!e.MoveNext())
+                    {
+                        Console.Error.WriteLine("Unterminated field");
+                        return;
+
+                    }
+                    fields.Add(f);
+                    f = default;
+                    if(e.Current.SymbolId==rbrace)
+                    {
+                        break;
+                    }
+                }
+                if (name != null)
+                {
+                    structs.Add(name, fields);
+                }
+            }
+        }
+        foreach(var s in structs)
+        {
+            if (s.Key == "data_packet" || s.Key == "command_packet" || s.Key == "config_packet")
+            {
+                foreach (var f in s.Value)
+                {
+                    Output.Write("\""+f.Name+"\", ");
+                    Output.Write("\"" + f.RawType + "\", ");
+                    Output.WriteLine(f.RawLength==0?4:f.RawLength);
+                }
+                Output.WriteLine();
+            }
+        }
+    }
+    
+    [FARule(@"\/\/[^\n]*")]  // 0
+    [FARule(@"\/\*",BlockEnd = @"\*\/")] // 1
+    [FARule(@"#[^\n]*")] // 2
+    [FARule(@"[ \r\n\t]+)")] // 3
+    [FARule(@"[A-Z_a-z][0-9A-Z_a-z]*")] // 4
+    [FARule(@"0|[1-9][0-9]*")] // 5
+    [FARule(@";")]
+    [FARule(@"\[")]
+    [FARule(@"\]")]
+    [FARule(@"\{")]
+    [FARule(@"\}")]
+    [FARule(@"\:\:")]
+    [FARule(@"\:")]
+    [FARule(@"\,")]
+    [FARule(@"\+")]
+    [FARule(@"\-")]
+    [FARule(@"=")]
+    internal static partial FATextReaderRunner LexRaw(TextReader reader);
+
+    static IEnumerable<FAMatch> Lex(TextReader reader)
+    {
+        FATextReaderRunner runner = LexRaw(reader);
+        var match = runner.NextMatch();
+        while(match.SymbolId!=-2)
+        {
+            if(match.SymbolId==-1 || match.SymbolId>3)
+            {
+                yield return match;
+            }
+            match = runner.NextMatch();
+        }
+    }
+    const int identifier = 4;
+    const int integer = 5;
+    const int semi = 6;
+    const int lbracket = 7;
+    const int rbracket = 8;
+    const int lbrace = 9;
+    const int rbrace = 10;
+    const int doublecolon = 11;
+    const int colon = 12;
+    const int comma = 13;
+    const int plus = 14;
+    const int minus = 15;
+    const int equals = 16;
+
+}
+
---
+++ h2csv/interface.h
@@ -0,0 +1,156 @@
+// XBee - ESP32 Interface Protocol
+// Rev 00.00.01
+// 08/01/23
+
+
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+#include <stdint.h>
+#include <stddef.h>
+
+enum struct STATUS_CODE : int32_t {
+    INVALID_COMMAND = -1,
+    SUCCESS = 0,
+};
+
+enum struct COMMAND_ID : int32_t {
+    ERROR = -1,
+    CONNECT = 1,
+    ACKNOWLEDGE,
+    DATA,
+    STATUS,
+    LOG,
+    CONNECTION,
+    CONFIG,
+    COMMAND
+};
+enum ACE_BOOL : uint32_t {
+    ACE_FALSE = 0,
+    ACE_TRUE = 1
+};
+
+// ====== Command Protocol ======
+    // each communication transaction begins with a command id (int)
+    // followed by 1 packet as indicated by the command field
+    // and then CRC-32 4-byte unsigned int followed by the payload
+struct connect_packet
+{
+    char unitname[16];                          // unitname | 16 bytes | string padded by zeros | Unique Unit Name
+    char host[128];                             // host | 128 bytes | string padded by zeros | Server Host URL
+    uint32_t port;                              // port | 4 bytes | 32-bit unsigned word | TCP/IP port #
+    char username[128];                         // username | 128 bytes | string padded by zeros | MQTT server username
+    char password[128];                         // password | 128 bytes | string padded by zeros | MQTT server password
+    char lastWillTopic[128];                    // lastWillTopic | 128 bytes | string padded by zeros | MQTT Last Will Topic address
+    uint32_t lastWillQos;                       // lastWillQos | 4 bytes | 32-bit unsigned word | Quality of Service Level for the Last Will Message
+    char lastWillMessage[128];                  // lastWillMessage | 128 bytes | string padded by zeros | Last Will Message
+    ACE_BOOL lastWillRetain;                    // lastWillRetain | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    ACE_BOOL cleanSession;                      // cleanSession | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+};
+
+
+struct acknowledge_packet {
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::ACKNOWLEDGE;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    STATUS_CODE status;                         // status | STATUS_CODE 32-bit unsigned word | 0 = Success, Negative Value = Various Errors
+};
+
+
+struct data_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::DATA;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         //  TopicName | 16 bytes | string padded by zeros | Topic to publish to
+    uint32_t qos;                               //  qos | 4 bytes | 32-bit unsigned word | Quality of Service
+    ACE_BOOL retainFlag;                        //  retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char timeStampUTC[32];                      //  timeStampUTC | 32 bytes | string padded by zeros |
+    ACE_BOOL powerOn;                           //  powerOn | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    ACE_BOOL ignitionOn;                        //  ignitionOn | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    uint32_t eventCode;                         //  eventCode | 4 bytes | 32-bit unsigned word | 
+    uint32_t cellStrength;                      //  cellStrength | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL alarmOn;                           //  alarmOn | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    uint32_t leftTemp;                          //  leftTemp | 4 bytes | 32-bit unsigned word | 
+    uint32_t rightTemp;                         //  rightTemp | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL stallSensorPresent;                //  stallSensorPresent | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    uint32_t stallCount;                        //  stallCount | 4 bytes | 32-bit unsigned word | 
+    uint32_t batteryVoltage;                    //  batteryVoltage | 4 bytes | 32-bit unsigned word | 
+    char doorPopUTC[32];                        //  doorPopUTC | 32 bytes | string padded by zeros |
+    uint32_t version;                           //  version | 4 bytes | 32-bit unsigned word | 
+
+};
+
+struct status_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::STATUS;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         //  TopicName | 16 bytes | string padded by zeros |
+    uint32_t qos;                               //  qos | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL retainFlag;                        //  retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char unitID[32];                            //  unitID | 32 bytes | string padded by zeros |
+    char unitname[16];                          //  unitname | 16 bytes | string padded by zeros |
+    char unitFirmwareVersion[16];               //  unitFirmwareVersion | 16 bytes | string padded by zeros |
+    char ctrlHeadSerialNumber[32];              //  ctrlHeadSerialNumber | 32 bytes | string padded by zeros |
+    char modemModel[16];                        //  modemModel | 16 bytes | string padded by zeros |
+    char modemFirmwareVersion[32];              //  modemFirmwareVersion | 32 bytes | string padded by zeros |
+    char carrierCode[8];                        //  carrierCode | 8 bytes | string padded by zeros |
+    char mobileEquipmentID[16];                 //  mobileEquipmentID | 16 bytes | string padded by zeros |
+    char integratedCircuitCardID[32];           //  integratedCircuitCardID | 32 bytes | string padded by zeros |
+    uint32_t doorPopCount;                      //  doorPopCount | 4 bytes | 32-bit unsigned word | 
+};
+
+struct log_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::LOG;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         //  TopicName | 16 bytes | string padded by zeros |
+    uint32_t qos;                               //  qos | 4 bytes | 32-bit unsigned word |
+    ACE_BOOL retainFlag;                        //  retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char timeStampUTC[32];                      //  timeStampUTC | 32 bytes | string padded by zeros |
+    uint32_t type;                              //  type | 4 bytes | 32-bit unsigned word |
+    char message[128];                          //  message | 128 bytes | string padded by zeros |
+};
+
+struct connection_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::CONNECTION;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         // TopicName | 16 bytes | string padded by zeros |
+    uint32_t qos;                               // qos | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL retainFlag;                        // retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char status[16];                            // status | 16 bytes | string padded by zeros |
+};
+
+struct config_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::CONFIG;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         // TopicName | 16 bytes | string padded by zeros |
+    uint32_t qos;                               // qos  | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL retainFlag;                        // retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char serverDomain[128];                     // serverDomain | 128 bytes | string padded by zeros |
+    char firmwareVersion[16];                   // firmwareVersion | 16 bytes | string padded by zeros |
+    char firmwareChecksum[32];                  // firmwareChecksum | 32 bytes | string padded by zeros |
+    char firmwareURL[128];                      // firmwareURL | 128 bytes | string padded by zeros |
+    uint32_t heartbeatInterval;                 // heartbeatInterval | 4 bytes | 32-bit unsigned word | 
+    uint32_t temperatureInterval;               // temperatureInterval | 4 bytes | 32-bit unsigned word | 
+    uint32_t temperatureDelta;                  // temperatureDelta | 4 bytes | 32-bit unsigned word | 
+    uint32_t connectTimeout;                    // connectTimeout | 4 bytes | 32-bit unsigned word | 
+    uint32_t registrationFailureLimit;          // registrationFailureLimit | 4 bytes | 32-bit unsigned word | 
+    uint32_t hotAlarmtemperature;               // hotAlarmtemperature | 4 bytes | 32-bit unsigned word | 
+    uint32_t coldAlarmTemperature;              // coldAlarmTemperature | 4 bytes | 32-bit unsigned word | 
+    uint32_t keepAliveInterval;                 // keepAliveInterval | 4 bytes | 32-bit unsigned word | 
+    uint32_t LoggingLevel;                      // LoggingLevel | 4 bytes | 32-bit unsigned word | 
+    uint32_t assignmentStatus;                  // assignmentStatus | 4 bytes | 32-bit unsigned word | 
+};
+
+struct command_packet
+{
+    constexpr static const COMMAND_ID cmd_ID = COMMAND_ID::COMMAND;
+    // uint32_t crc; (prepended to packet)      // CRC | 4 bytes | 32-bit unsigned word  | Indicates the CRC-32 value for the packet
+    char TopicName[16];                         // TopicName | 16 bytes | string padded by zeros |
+    uint32_t qos;                               // qos | 4 bytes | 32-bit unsigned word | 
+    ACE_BOOL retainFlag;                        // retainFlag | ACE_BOOL 32-bit unsigned word | 0 = False, Non-Zero = True
+    char command[128];                          // command | 128 bytes | string padded by zeros |
+};
+
+#endif // INTERFACE_H
